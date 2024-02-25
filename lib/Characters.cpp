5f672d8f7d4d50a207ccfd24a15b8c112f55c9c8
pkutyl <01168894@pw.edu.pl>
2024-02-25 19:30:27 +0100

    Update test for empty vector

--- lib/Characters.cpp
+++ lib/Characters.cpp
@@ -14,3 +14,27 @@ double Characters::letterFrequency() const {
                     [](unsigned char ch) { return std::isalpha(ch); });
   return (double)letters / count();
 }
+
+double Characters::frequencyCertainLetter(const char& letter) const {
+  const auto letters = std::count_if(
+      std::begin(data), std::end(data), [letter](unsigned char ch) {
+        return std::tolower(ch) == std::tolower(letter);
+      });
+  return (double)letters / count();
+}
+
+void Characters::checkIfEmpty() const {
+  if (data.empty()) {
+    throw std::invalid_argument("Vector is empty");
+  }
+}
+double Characters::getLowestFrequency() const {
+  checkIfEmpty();
+  double min_frequency = 1;
+  for (auto letter : data) {
+    if (frequencyCertainLetter(letter) < min_frequency) {
+      min_frequency = frequencyCertainLetter(letter);
+    }
+  }
+  return min_frequency;
+}
